ab1146de4abbf7b7000fa058297e1cf82bcf90fa
Aditya Dutta <adi22dutta@gmail.com>
2025-08-04 15:34:16 +0530

    Update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim", lazy = true },
-  { "folke/tokyonight.nvim", opts = { style = "night" } },
-  { "rebelot/kanagawa.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    -- add gruvbox
+    { "ellisonleao/gruvbox.nvim", lazy = true },
+    { "folke/tokyonight.nvim", opts = { style = "night" } },
+    { "rebelot/kanagawa.nvim" },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "tokyonight",
+        },
     },
-  },
 }
